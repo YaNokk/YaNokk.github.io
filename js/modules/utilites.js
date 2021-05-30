78f4235695187de48fc2cdcd568ef7a196b3db00
YaNokk <67015624+YaNokk@users.noreply.github.com>
2021-05-30 02:36:15 +0200

    Add files via upload

--- js/modules/utilites.js
+++ js/modules/utilites.js
@@ -18,24 +18,6 @@ function chartSort(a, b) {
     return 0; 
 }
 
-function sortTableByColumn(table, column, asc = true) {
-    const dirModifier = asc ? 1 : -1;
-    const tBody = table.tBodies[0];
-    const rows = Array.from(tBody.querySelectorAll("tr"));
-    const sortedRows = rows.sort((a, b) => {
-        const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim() + a.querySelector('input').value.trim();
-        const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim() + b.querySelector('input').value.trim();
-        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
-    });
-    while (tBody.firstChild) {
-        tBody.removeChild(tBody.firstChild);
-    }
-    tBody.append(...sortedRows);
-    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
-    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
-    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
-}
-
 function validator(dataArray){
     function createNotification(type, message){
         const notification = document.createElement('div');
@@ -82,12 +64,7 @@ function validator(dataArray){
     })
     return status;
 }
-
 function chartformatDate(date){
     return date.split('-').reverse().join('/')
 }
-function inputformatDate(date){
-    return date.split('/').reverse().join('-')
-}
-
-export {getRandomColor, chartSort, validator, chartformatDate, inputformatDate, sortTableByColumn};
+export {getRandomColor, chartSort, validator};
